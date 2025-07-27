d532615050dc413221c3657013457200143f6e5c
Kartik Mankar <117511537+KartikM83@users.noreply.github.com>
2025-07-27 18:41:08 +0530

    Update LeaveType.java

--- Backend/src/main/java/org/kartikmankar/backend/entity/LeaveType.java
+++ Backend/src/main/java/org/kartikmankar/backend/entity/LeaveType.java
@@ -6,29 +6,8 @@ import lombok.Getter;
 
 @Getter
 public enum LeaveType {
-    SICK("Sick Leaves"),
-    VACATION("Vacation Leaves"),
-    OTHER("Other Leaves");
-
-    private final String label;
-
-    LeaveType(String label) {
-        this.label = label;
-    }
-
-    @JsonValue
-    public String getLabel() {
-        return label;
-    }
-
-    @JsonCreator
-    public static LeaveType fromLabel(String label) {
-        for (LeaveType type : LeaveType.values()) {
-            if (type.getLabel().equalsIgnoreCase(label)) {
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("Invalid leave type: " + label);
-    }
+    SICK,
+    VACATION,
+    OTHER;
 }
 
